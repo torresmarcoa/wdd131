6d057dd77ca7f94830e0e13d79dd864c50a8cab4
torresmarcoa <marcotorresaceves@gmail.com>
2024-06-12 23:58:27 -0600

    css included - js added

---
+++ scripts/getdates.js
@@ -0,0 +1,4 @@
+
+document.querySelector("#currentyear").textContent = new Date().getFullYear();
+
+document.getElementById('lastModified').textContent = "Last Modification: " + new Date(document.lastModified).toLocaleString();
