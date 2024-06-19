e9c2bfeb9a2916bf196e6758d015a72a8cd44226
torresmarcoa <marcotorresaceves@gmail.com>
2024-06-19 17:07:18 -0600

    activity w2 added

---
+++ week02/scripts/bom.js
@@ -0,0 +1,29 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list");
+
+
+button.addEventListener("click", function () {
+
+    if (input.value.trim() !== "") {
+        const li = document.createElement("li");
+        li.textContent = input.value;
+
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = "❌";
+        deleteButton.addEventListener("click", function () {
+            list.removeChild(li);
+            input.focus();
+        });
+        li.append(deleteButton);
+        list.append(li);
+
+        input.value = "";
+        input.focus();
+    }
+
+    input.focus();
+});
+
+
+
