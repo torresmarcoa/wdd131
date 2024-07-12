2da59a688cebb4bb0957e065e1c7820ec6b28c32
torresmarcoa <marcotorresaceves@gmail.com>
2024-07-11 23:21:08 -0600

    form completed

---
+++ scripts/form.js
@@ -0,0 +1,38 @@
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ];
+
+addProducts()
+
+function addProducts() {
+    products.forEach(function (product) {
+        const opt = document.createElement("option");
+        opt.value = product.id;
+        opt.textContent = product.name;
+        document.getElementById("productSelect").appendChild(opt);
+    });
+}
